aa8dc7e0d2cc7240a1e26cc921fcdb80cab1d43c
shachinc <shachinc393@gmail.com>
2026-03-24 15:15:32 -0400

    Added user search controller

---
+++ src/main/java/uga/menik/csx370/controllers/UserSearchController.java
@@ -0,0 +1,53 @@
+package uga.menik.csx370.controllers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import uga.menik.csx370.models.Post;
+import uga.menik.csx370.utility.Utility;
+
+/**
+ * Handles /hashtagsearch URL and possibly others.
+ * At this point no other URLs.
+ */
+@Controller
+@RequestMapping("/usersearch")
+public class UserSearchController {
+
+    /**
+     * This function handles the /hashtagsearch URL itself.
+     * This URL can process a request parameter with name hashtags.
+     * In the browser the URL will look something like below:
+     * http://localhost:8081/hashtagsearch?hashtags=%23amazing+%23fireworks
+     * Note: the value of the hashtags is URL encoded.
+     */
+    @GetMapping
+    public ModelAndView webpage(@RequestParam(name = "users") String users) {
+        System.out.println("User is searching: " + users);
+
+        // See notes on ModelAndView in BookmarksController.java.
+        ModelAndView mv = new ModelAndView("posts_page");
+
+        // Following line populates sample data.
+        // You should replace it with actual data from the database.
+        List<Post> posts = Utility.createSamplePostsListWithoutComments();
+        mv.addObject("posts", posts);
+
+        // If an error occured, you can set the following property with the
+        // error message to show the error message to the user.
+        // String errorMessage = "Some error occured!";
+        // mv.addObject("errorMessage", errorMessage);
+
+        // Enable the following line if you want to show no content message.
+        // Do that if your content list is empty.
+        // mv.addObject("isNoContent", true);
+
+        return mv;
+    }
+
+}
